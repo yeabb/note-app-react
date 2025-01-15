68f7bdffdf6b50339570941e4d0750b94ae8f69f
Yeabkal Biru <yeabkalbiru@gmail.com>
2025-01-14 23:44:12 -0800

    setup idle form structure

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from "react-router-dom"
+import { NewNote } from "./NewNote"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id"> 
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  )
+  
+}
+
+export default App
