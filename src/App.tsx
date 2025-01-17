c084fa2fd556251c23cd8d7cb93ddbf7d671c685
Yeabkal Biru <yeabkalbiru@gmail.com>
2025-01-17 01:41:07 -0800

    🗃️ Local storage works now

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
 import { NewNote } from "./NewNote"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import {v4 as uuidV4} from "uuid"
 
 export type Note = {
   id: string
@@ -21,7 +24,7 @@ export type Tag = {
 
 export type RawNote = {
   id: string
-}
+} & RawNoteData
 
 export type RawNoteData = {
   title: string
@@ -32,11 +35,28 @@ export type RawNoteData = {
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return  { ...note, tags: tags.filter(tag => note.tagsIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  function onCreateNote({tags, ...data}: NoteData) {
+    setNotes(prevNotes => {
+      return [...prevNotes, { ...data, id: uuidV4(), tagsIds: tags.map(tag => tag.id)}]
+    })
+  }
+  
+  function addTag(tag: Tag) {
+    setTags(prev => [...prev, tag])
+  }
+
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
         <Route path="/:id"> 
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
