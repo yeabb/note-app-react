b37a4f0a3d7ce8e055b6c08e5a0811baa60127bf
Yeabkal Biru <yeabkalbiru@gmail.com>
2025-01-18 02:31:54 -0800

    🐛 Fix NoteCard -> Note navigation bug

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -83,7 +83,7 @@ export function NoteList({availableTags, notes}: NoteListProps) {
 }
 
 function NoteCard({ id, title, tags }: SimplifiedNote) {
-    return <Card as={Link} to={"/${id}"} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
+    return <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
         <Card.Body>
             <Stack gap={2} className="align-items-center justify-content-center h-100">
                 <span className="fs-5">{title}</span>
